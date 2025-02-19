68d7c124c76c1487b9f1f2feb32d1d2460a5cf09
Jashen-Bright <84370381+Jashen-Bright@users.noreply.github.com>
2025-02-19 09:55:11 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+def palindrome(n):
+    if n==n[::-1]:
+        print("Palindrome number!")
+    else: 
+        print("Not a palindrome number")
+def main():
+    n=input("Enter number: ")
+    palindrome(n)
+main()
